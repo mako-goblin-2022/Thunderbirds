c56282df627f3150ad82c632dd71d790d19479aa
Jillian <19513426+FireDivine@users.noreply.github.com>
2022-06-03 11:21:30 +1200

    server now shows all birds

--- routes/birds.js
+++ routes/birds.js
@@ -6,52 +6,39 @@ const router = express.Router()
 
 router.get('/', (req, res) => {
   readBirds((birds) => {
-    res.render('home', birds)
+    res.render('home', {bird:birds.Birds})
   })
 })
 
 router.get('/:id', (req, res) => {
   readBirds((birds) => {
-    console.log(birds)
     const getBirds = birds.birds.find((birds) => birds.id == req.params.id)
-    console.log(getBirds)
     res.render('details', getBirds)
   })
 })
 router.get('/:id/edit', (req, res) => {
-  readPuppies((birds) => {
-    console.log(birds)
+  readBirds((birds) => {
     const getBirds = birds.birds.find((birds) => birds.id == req.params.id)
-    console.log(getBirds)
     res.render('edit', getBirds)
   })
 })
-
-// router.get('/add', (req, res) => {
-//   res.render('.puppiesForm')
-// })
-//POST /puppies/:id/edit
 router.post('/:id/edit', (req, res) => {
-  const newPuppies = req.body
-  readPuppies((puppies) => {
-    const puppyIndex = puppies.puppies.findIndex((puppies) => puppies.id == req.params.id)
-    puppies.puppies[puppyIndex] = {...newPuppies, id: req.params.id}
-    const data = JSON.stringify(puppies, null, 2)
+  const newBird = req.body
+  readBirds((bird) => {
+    const birdIndex = bird.bird.findIndex(bird => bird.id == req.params.id)
+    bird.bird[birdIndex] = {...newBird, id: req.params.id}
+    const data = JSON.stringify(bird, null, 2)
     fs.writeFile(path.resolve('./data.json'), data, (err) => {
       console.log(err)
-      res.redirect('/puppies')
+      res.redirect('/birds')
     })
   })
 })
 
-
-
-
-
-function readPuppies(callback) {
+function readBirds(callback) {
   fs.readFile('./data.json', 'utf-8', (err, data) => {
-    let puppies = JSON.parse(data)
-    callback(puppies)
+    let bird = JSON.parse(data)
+    callback(bird)
   })
 }
 
--- server.js
+++ server.js
@@ -1,22 +1,28 @@
 const express = require('express')
 const hbs = require('express-handlebars')
 
-//const BirdRoutes = require('./routes/bird')
+const BirdRoutes = require('./routes/birds')
 
 const server = express()
 
 // Server configuration
 server.use(express.static('public'))
 server.use(express.urlencoded({ extended: true }))
-
+const bird = require('./data.json')
 // Handlebars configuration
 server.engine('hbs', hbs.engine({ extname: 'hbs' }))
 server.set('view engine', 'hbs')
 
 // Your routes/router(s) should go here
-//server.use('/bird', BirdRoutes)
+server.use('/bird', BirdRoutes)
 server.get('/', (req, res) => {
-  res.render("home")
+  // const viewData={
+  //   title:"birds home page",
+  //   bird:bird.Birds
+  // }
+  // console.log(viewData)
+  // res.render('home',viewData)
+  res.redirect('/bird')
   // readBirds((birds) => {
   //   res.render('home', birds)
   // })
